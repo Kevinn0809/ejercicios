0243e1a71e0c9ae4ddf9b2aa915b24e1fc731cd0
Kevinn0809 <kpinzonnino@gmail.com>
2023-09-09 10:55:14 -0500

    Se fixea error (No sonaban el piano)

--- taller-poo/js/piano.js
+++ taller-poo/js/piano.js
@@ -12,12 +12,12 @@ class teclaPiano {
 }
 
 // Crea instancias de las teclas del piano con diferentes archivos de sonido y botones HTML correspondientes
-const teclaDo = new teclaPiano('/assets/notas/nota1.mp3', document.querySelector("#do"))
-const teclaRe = new teclaPiano('/assets/notas/nota2.mp3', document.querySelector("#re"))
-const teclaMi = new teclaPiano('/assets/notas/nota3.mp3', document.querySelector("#mi"))
-const teclaFa = new teclaPiano('/assets/notas/nota4.mp3', document.querySelector("#fa"))
-const teclaSol = new teclaPiano('/assets/notas/nota5.mp3', document.querySelector("#sol"))
-const teclaLa = new teclaPiano('/assets/notas/nota6.mp3', document.querySelector("#la"))
-const teclaSi = new teclaPiano('/assets/notas/nota7.mp3', document.querySelector("#si"))
-const teclaDoEnd = new teclaPiano('/assets/notas/nota8.mp3', document.querySelector("#doEnd"))
+const teclaDo = new teclaPiano('./assets/notas/nota1.mp3', document.querySelector("#do"))
+const teclaRe = new teclaPiano('./assets/notas/nota2.mp3', document.querySelector("#re"))
+const teclaMi = new teclaPiano('./assets/notas/nota3.mp3', document.querySelector("#mi"))
+const teclaFa = new teclaPiano('./assets/notas/nota4.mp3', document.querySelector("#fa"))
+const teclaSol = new teclaPiano('./assets/notas/nota5.mp3', document.querySelector("#sol"))
+const teclaLa = new teclaPiano('./assets/notas/nota6.mp3', document.querySelector("#la"))
+const teclaSi = new teclaPiano('./assets/notas/nota7.mp3', document.querySelector("#si"))
+const teclaDoEnd = new teclaPiano('./assets/notas/nota8.mp3', document.querySelector("#doEnd"))
 console.log(teclaDoEnd);
